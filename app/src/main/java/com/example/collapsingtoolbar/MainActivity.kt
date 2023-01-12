ed455ecf08648db8fbacd2e58013b4eeab670738
Muhammad Beni Fajri <67430854+bennyfajri@users.noreply.github.com>
2023-01-12 12:40:43 +0700

    (not)proper way collapsing toolbar

--- app/src/main/java/com/example/collapsingtoolbar/MainActivity.kt
+++ app/src/main/java/com/example/collapsingtoolbar/MainActivity.kt
@@ -1,19 +1,26 @@
 package com.example.collapsingtoolbar
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.provider.CalendarContract.Colors
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.view.Window
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.example.collapsingtoolbar.databinding.ActivityMainBinding
 import com.google.android.material.appbar.AppBarLayout
-import android.widget.Toast
-
-
+import kotlin.math.abs
 
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     var appbarExtended = true
     lateinit var collapseMenu: Menu
 
@@ -25,20 +32,30 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         binding.appbar.addOnOffsetChangedListener(
-            AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
-                if(Math.abs(verticalOffset) > 200){
-                    appbarExtended = false
-                }else{
-                    appbarExtended = true
-                }
+            AppBarLayout.OnOffsetChangedListener { _, verticalOffset ->
+                appbarExtended = abs(verticalOffset) <= 433
+                Log.d(TAG, "verticalOffset: $verticalOffset")
+//                setLightStatusBar(abs(verticalOffset) >= 433)
                 invalidateOptionsMenu()
             }
         )
 
     }
 
+    private fun setLightStatusBar(expandState: Boolean) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val window: Window = window
+            val decorView: View = window.decorView
+            val wic = WindowInsetsControllerCompat(window, decorView)
+            wic.isAppearanceLightStatusBars = expandState // true or false as desired.
+
+            // And then you can set any background color to the status bar.
+            window.statusBarColor = Color.TRANSPARENT
+        }
+    }
+
     override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
-        if(collapseMenu != null && (!appbarExtended || collapseMenu.size() != 1)){
+        if(!appbarExtended || collapseMenu.size() != 1){
             collapseMenu.add("Add")
                 .setIcon(R.drawable.ic_add)
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM)
@@ -48,7 +65,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu, menu)
-        collapseMenu = menu!!
+        if (menu != null) {
+            collapseMenu = menu
+        }
         return true
     }
 
@@ -63,5 +82,8 @@ class MainActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
+    companion object {
+        const val TAG = "Response:::"
+    }
 
 }
